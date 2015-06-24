404bb18ddd28c2ea087f74d3385900fcb8d860ff
Andrew Hudson <ahudson@gmail.com>
2015-06-24 16:49:34 +0100

    update cache version number

--- js/serviceWorkers.js
+++ js/serviceWorkers.js
@@ -5,11 +5,13 @@ importScripts('/js/vendor/serviceworker-cache-polyfill.js');
 
 console.log('hello Worlds!');
 
-var cacheName = 'bigandy-cache-v15';
+var cacheName = 'bigandy-cache-v16';
 var filesToCache = [
     // Stylesheets
     '/css/style.css',
     '/css/font.css',
+
+    // Favicon
     '/favicon.ico',
 
     // Posts
