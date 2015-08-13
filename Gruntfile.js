1f3c9b9950944c5babd144204e81a1e43eda99af
Andrew Hudson <ahudson@gmail.com>
2015-08-13 14:55:22 +0100

    move angular code back inline: easier to edit while doing demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,43 +8,13 @@ module.exports = function(grunt) {
             dest: "_site",
             src: "source"
         },
-        // let us know if our JS is sound
-        jshint: {
-            options: {
-                "bitwise": true,
-                "browser": true,
-                "curly": true,
-                "eqeqeq": true,
-                "eqnull": true,
-                "es5": true,
-                "esnext": true,
-                "immed": true,
-                "jquery": true,
-                "latedef": true,
-                "newcap": true,
-                "noarg": true,
-                "node": true,
-                "strict": false,
-                "trailing": true,
-                "undef": true,
-                "globals": {
-                    "jQuery": true,
-                    "alert": true
-                }
-            },
-            all: [
-                'Gruntfile.js',
-                'js/demos/*.js'
-            ]
-        },
 
         // concatenation and minification all in one
         uglify: {
             dist: {
                 files: {
-                    'js/build/angular/posts.js': [
-                        'bower_components/angular/angular.min.js',
-                        'js/angular/posts.js'
+                    'js/build/angular/angular.js': [
+                        'bower_components/angular/angular.min.js'
                     ]
                 }
             }
@@ -74,7 +44,7 @@ module.exports = function(grunt) {
                 files: [
                     '<%= jshint.all %>'
                 ],
-                tasks: ['jshint', 'uglify', 'requirejs']
+                tasks: ['jshint', 'js', 'requirejs']
             }
         }
     });
@@ -86,8 +56,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-uncss');
-
-
 
     // register task
     grunt.registerTask('default', [
