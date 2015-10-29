dbf259e17c8d9c405335df30cf4a7f1a7360d38b
Andrew Hudson <ahudson@gmail.com>
2015-10-29 14:20:33 +0000

    change serviceWorkers -> serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,7 +3,7 @@ layout: null
 ---
 importScripts('/js/vendor/serviceworker-cache-polyfill.js');
 
-var cacheName = 'bigandy-cache-v22';
+var cacheName = 'bigandy-cache-v23';
 var filesToCache = [
     // Stylesheets
     '/css/style.css',
