956c795f8f830173cced97798e2b52fa3b810fc1
Andrew Hudson <ahudson@gmail.com>
2017-03-13 21:39:03 +0000

    wow found a cool way to dev

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,64 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const cssFiles = '_sass/**/*.?(s)css';
 
 const sourcemaps = require('gulp-sourcemaps');
+const postcss      = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const child = require('child_process');
+const gutil = require('gulp-util');
+const browserSync = require('browser-sync').create();
+
+const siteRoot = '_site';
 
 var browsers = ['last 1 version'];
 
-gulp.task('sass', () => {
-	gulp.src([
-			'./_sass/style.scss',
-		])
-		// .pipe(sourcemaps.init())
-		// .pipe(autoprefixer(browsers))
-		.pipe(sass({
-			'outputStyle': 'compressed',
-		}).on('error', sass.logError))
-		// .pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./css'));
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build',
+    '--watch',
+    '--incremental',
+    '--drafts'
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
 });
 
-// // Rerun the task when a file changes
-gulp.task('watch', () => {
-	gulp.watch('_sass/**/*', ['sass']);
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+
+  gulp.watch(cssFiles, ['sass']);
+});
+
+gulp.task('sass', () => {
+	return gulp.src([
+			'./_sass/style.scss',
+		])
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			'outputStyle': 'compressed',
+		}).on('error', (e) => console.log(e)))
+        .pipe(postcss([ autoprefixer(browsers) ]))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./css'));
 });
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', [
 	'sass',
-	'watch'
+	'serve',
+	'jekyll'
 ]);
